a2e27f58470b28314ce4c61a408fe1086eff232e
zhoulin <Fancyzero@live.cn>
2016-08-10 11:39:06 +0800

    fix warnings and typos

--- Main.py
+++ Main.py
@@ -2,18 +2,18 @@
 from PIL import Image
 import math
 
-img = Image.new( "RGB",(32,32) )
-amap = img.load()
+img = Image.new("RGB", (32, 32))
+pixels = img.load()
 
 
-def dda(start, end, amap, color):
+def dda(start, end, a_map, color):
     exchange_xy = False
     flip_x = False
     flip_y = False
 
     x = end[0] - start[0]
     y = end[1] - start[1]
-    print x,y
+    print x, y
     if y == 0:
         slope = 1
     else:
@@ -22,20 +22,20 @@ def dda(start, end, amap, color):
     if slope > 1:
         exchange_xy = True
         x, y = y, x
-        slope = abs(float(x)/y )
+        slope = abs(float(x) / y)
 
     if x < 0:
         flip_x = True
-        x=-x
+        x = -x
     if y < 0:
         flip_y = True
-        y=-y
+        y = -y
 
-    dda_impl(start, x, y, slope, amap, exchange_xy, flip_x, flip_y, color)
+    dda_impl(start, x, y, slope, a_map, exchange_xy, flip_x, flip_y, color)
 
 
-def dda_impl(start, x_diff, y_diff, slope, amap, exchange_xy, flip_x, flip_y,color):
-    print start, x_diff,y_diff,slope, exchange_xy,flip_x,flip_x
+def dda_impl(start, x_diff, y_diff, slope, a_map, exchange_xy, flip_x, flip_y, color):
+    print start, x_diff, y_diff, slope, exchange_xy, flip_x, flip_x
     aa = slope
     x = 0
     y = 0
@@ -47,8 +47,8 @@ def dda_impl(start, x_diff, y_diff, slope, amap, exchange_xy, flip_x, flip_y,col
         if flip_y:
             yw = -yw
         if exchange_xy:
-            xw,yw = yw,xw
-        amap[yw + start[1],xw + start[0]] = color
+            xw, yw = yw, xw
+        a_map[yw + start[1], xw + start[0]] = color
         aa += slope
         if aa > 1:
             aa -= 1
@@ -56,10 +56,10 @@ def dda_impl(start, x_diff, y_diff, slope, amap, exchange_xy, flip_x, flip_y,col
         y += 1
 
 
-for i in range(0,16):
-    x=math.sin(i*3.1415926/8)*10+16
-    y=math.cos(i*3.1415926/8)*10+16
-    dda((16, 16), (x, y), amap,(int(128*i/16.0),int(128*i/16.0),int(128*i/16.0)))
+for i in range(0, 16):
+    xe = math.sin(i * 3.1415926 / 8) * 10 + 16
+    ye = math.cos(i * 3.1415926 / 8) * 10 + 16
+    dda((16, 16), (xe, ye), pixels, (int(128 * i / 16.0), int(128 * i / 16.0), int(128 * i / 16.0)))
 img.show()
 #
 # output = ""
