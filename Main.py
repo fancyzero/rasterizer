86d682dfda1c008dbb218a0d077319443ab06f41
zhoulin <Fancyzero@live.cn>
2016-08-10 17:39:12 +0800

    optimize and using integer instead of float

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@
 from PIL import Image
 import math
 
-img = Image.new("RGB", (32, 32))
+picture_width = 512
+picture_height = 512
+img = Image.new("RGB", (picture_width, picture_height))
 pixels = img.load()
 
 
@@ -13,16 +15,15 @@ def dda(start, end, a_map, color):
 
     x = end[0] - start[0]
     y = end[1] - start[1]
-    print x, y
-    if y == 0:
+
+    if x == 0:
         slope = 1
     else:
-        slope = abs(float(x) / y)
+        slope = abs(float(y) / x)
 
-    if slope > 1:
+    if slope >= 1:
         exchange_xy = True
         x, y = y, x
-        slope = abs(float(x) / y)
 
     if x < 0:
         flip_x = True
@@ -31,15 +32,14 @@ def dda(start, end, a_map, color):
         flip_y = True
         y = -y
 
-    dda_impl(start, x, y, slope, a_map, exchange_xy, flip_x, flip_y, color)
+    dda_impl(start, x, y, a_map, exchange_xy, flip_x, flip_y, color)
 
 
-def dda_impl(start, x_diff, y_diff, slope, a_map, exchange_xy, flip_x, flip_y, color):
-    print start, x_diff, y_diff, slope, exchange_xy, flip_x, flip_x
-    aa = slope
+def dda_impl(start, x_diff, y_diff, a_map, exchange_xy, flip_x, flip_y, color):
+    error = x_diff / 2
     x = 0
     y = 0
-    while y <= y_diff:
+    while x <= x_diff:
         xw = x
         yw = y
         if flip_x:
@@ -48,18 +48,24 @@ def dda_impl(start, x_diff, y_diff, slope, a_map, exchange_xy, flip_x, flip_y, c
             yw = -yw
         if exchange_xy:
             xw, yw = yw, xw
-        a_map[yw + start[1], xw + start[0]] = color
-        aa += slope
-        if aa > 1:
-            aa -= 1
-            x += 1
-        y += 1
+        a_map[xw + start[0], yw + start[1]] = color
+        print error, x, y
+        error -= y_diff
+        if error < 0:
+            error += x_diff
+            y += 1
+        x += 1
 
 
-for i in range(0, 16):
-    xe = math.sin(i * 3.1415926 / 8) * 10 + 16
-    ye = math.cos(i * 3.1415926 / 8) * 10 + 16
-    dda((16, 16), (xe, ye), pixels, (int(128 * i / 16.0), int(128 * i / 16.0), int(128 * i / 16.0)))
+for i in range(1, 16):
+    xe = (math.sin(i * 3.1415926 / 8) * 21 + picture_width / 2)
+    ye = (math.cos(i * 3.1415926 / 8) * 21 + picture_height / 2)
+    xe = int(xe)
+    ye = int(ye)
+    dda((picture_width / 2, picture_height / 2), (xe, ye), pixels,
+        (int(128 * i / 16.0) + 128, int(128 * i / 16.0) + 128, int(128 * i / 16.0) + 128))
+    if i == 1:
+        break
 img.show()
 #
 # output = ""
@@ -72,3 +78,7 @@ img.show()
 #     output += "\n"
 #
 # print output
+
+
+
+print i
